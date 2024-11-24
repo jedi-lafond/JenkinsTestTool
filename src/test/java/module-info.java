dcefa9b39fda53884082387d1184148c74b547b3
Jedidiah L <jedlafond@gmail.com>
2024-11-23 17:38:49 -0800

    test

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -2,4 +2,6 @@ module JUnitTesting.test {
     requires JUnitTesting.main;
     requires org.junit.jupiter.api;
     requires org.mockito;
+    requires javafx.controls;
+    requires javafx.base;
 }
