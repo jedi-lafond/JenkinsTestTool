8b35dc5462c16c8acb537840ad56dad1ed38f600
Jedidiah L <jedlafond@gmail.com>
2024-11-23 18:01:55 -0800

    test

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -3,8 +3,7 @@
  */
 module JenkinsTestTool.test {
     requires JenkinsTestTool.main;
-    requires org.junit.jupiter.api;
-    requires org.junit.jupiter.engine;
+    requires org.junit.jupiter;
     requires org.mockito;
 
     requires javafx.controls;
